ecef4f0df76b445c352141aa0a5bc558b0fa9eef
soywiz <soywiz@gmail.com>
2020-03-18 17:29:11 +0100

    More work

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const exec = util.promisify(require('child_process').exec);
 		const LOCAL_PATH = config.LOCAL_PATH
 		const CACHE_PATH = config.CACHE_PATH
 		const cmd = `mkdir -p ${CACHE_PATH} && mkdir -p ${LOCAL_PATH} && cp -rf ${CACHE_PATH}/ ${LOCAL_PATH}/`;
+		console.log(cmd);
 		const { stdout, stderr } = await exec(cmd);
 		console.log(stdout);
 		console.log(stderr);
--- post.js
+++ post.js
@@ -7,6 +7,7 @@ const exec = util.promisify(require('child_process').exec);
                 const LOCAL_PATH = config.LOCAL_PATH
                 const CACHE_PATH = config.CACHE_PATH
 		const cmd = `cp -rf $LOCAL_PATH/ $CACHE_PATH/`;
+		console.log(cmd);
 		const { stdout, stderr } = await exec(cmd);
 		console.log(stdout);
 		console.log(stderr);
