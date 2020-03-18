56eff469f830ff8950d37f23f2098f1ebf79e30c
soywiz <soywiz@gmail.com>
2020-03-18 17:28:10 +0100

    More work

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const exec = util.promisify(require('child_process').exec);
 	try {
 		const LOCAL_PATH = config.LOCAL_PATH
 		const CACHE_PATH = config.CACHE_PATH
-		const { stdout, stderr } = await exec(`mkdir -p ${CACHE_PATH} && mkdir -p ${LOCAL_PATH} && cp -rf ${CACHE_PATH}/ ${LOCAL_PATH}/`);
+		const cmd = `mkdir -p ${CACHE_PATH} && mkdir -p ${LOCAL_PATH} && cp -rf ${CACHE_PATH}/ ${LOCAL_PATH}/`;
+		const { stdout, stderr } = await exec(cmd);
 		console.log(stdout);
 		console.log(stderr);
 	} catch (e) {
--- post.js
+++ post.js
@@ -6,7 +6,8 @@ const exec = util.promisify(require('child_process').exec);
 	try {
                 const LOCAL_PATH = config.LOCAL_PATH
                 const CACHE_PATH = config.CACHE_PATH
-		const { stdout, stderr } = await exec(`cp -rf $LOCAL_PATH/ $CACHE_PATH/`);
+		const cmd = `cp -rf $LOCAL_PATH/ $CACHE_PATH/`;
+		const { stdout, stderr } = await exec(cmd);
 		console.log(stdout);
 		console.log(stderr);
 	} catch (e) {
